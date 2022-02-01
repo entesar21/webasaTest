3c28b4f6adb253915eb78f78b6a18c8e46c00664
entesar torfizadegan <entesar.torfizadegan@gmail.com>
2022-02-01 13:03:22 +0330

    add social media fields to footer and update patch API (partial edit API)

--- footer/models.py
+++ footer/models.py
@@ -4,11 +4,17 @@ from datetime import datetime
 # Create your models here.
 
 class Footer(models.Model):
-    footer_about_us= models.TextField(default='default about us')
-    footer_telegram = models.CharField(max_length=120,default='http://telegram.me/webasa')
-    footer_insta = models.CharField(max_length=120,default='http://insta.me/webasa')
-    footer_whats_app = models.CharField(max_length=120,default='http://whatsapp.me/webasa')
-    footer_phone = models.IntegerField(default='02598653269')
+    footer_about_us= models.TextField(default='default about us',blank=True,null=True)
+    footer_telegram = models.CharField(max_length=120,default='http://www.telegram.me/webasa',blank=True,null=True)
+    footer_insta = models.CharField(max_length=120,default='https://www.instagram.com/webasa',blank=True,null=True)
+    footer_whats_app = models.CharField(max_length=120,default='http://www.whatsapp.me/webasa',blank=True,null=True)
+    footer_aparat = models.CharField(max_length=120,default='https://www.aparat.com/webasa',blank=True,null=True)
+    footer_youtube = models.CharField(max_length=120,default='https://www.youtube.com/webasa',blank=True,null=True)
+    footer_twitter = models.CharField(max_length=120,default='https://twitter.com/webasa',blank=True,null=True)
+    footer_linkedin = models.CharField(max_length=120,default='https://www.aparat.com/webasa',blank=True,null=True)
+    footer_facebook = models.CharField(max_length=120,default='https://www.facebook.com/webasa',blank=True,null=True)
+    footer_phone = models.IntegerField(default='02598653269',blank=True,null=True)
+    footer_address = models.IntegerField(default='02598653269',blank=True,null=True)
     created_at = models.DateTimeField(default=datetime.now)
 
 
--- footer/views.py
+++ footer/views.py
@@ -39,7 +39,15 @@ class EditFooter(APIView):
         footer_obj.footer_telegram = data.get("footer_telegram",footer_obj.footer_telegram)
         footer_obj.footer_insta = data.get("footer_insta",footer_obj.footer_insta)
         footer_obj.footer_whats_app = data.get("footer_whats_app",footer_obj.footer_whats_app)
-        footer_obj.footer_phone = data.get("footer_phone",footer_obj.footer_phone)
+
+
+        footer_obj.footer_aparat = data.get("footer_aparat",footer_obj.footer_aparat)
+        footer_obj.footer_youtube = data.get("footer_youtube",footer_obj.footer_youtube)
+        footer_obj.footer_twitter = data.get("footer_twitter",footer_obj.footer_twitter)
+        footer_obj.footer_linkedin = data.get("footer_linkedin",footer_obj.footer_linkedin)
+        footer_obj.footer_address = data.get("footer_address",footer_obj.footer_address)
+        footer_obj.footer_phone = data.get("footer_phone", footer_obj.footer_phone)
+        footer_obj.footer_facebook = data.get("footer_facebook", footer_obj.footer_facebook)
 
         footer_obj.save()
         serializers = FooterSerializer(footer_obj)
