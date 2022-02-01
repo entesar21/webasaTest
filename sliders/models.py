ba0d42f88a3888ac40e9cccfa8f358c4a634613f
entesar torfizadegan <entesar.torfizadegan@gmail.com>
2022-02-01 10:58:07 +0330

    adding slider_link,slider_text,slider_button_text fields to sliders model

--- sliders/models.py
+++ sliders/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Slider(models.Model):
     slider_title = models.CharField(max_length=120)
     slider_alt = models.TextField(default='')
+    slider_link = models.CharField(max_length=120,default='',blank=True,null=True)
+    slider_button_text = models.CharField(max_length=120,default='',blank=True,null=True)
+    slider_text = models.TextField(default='',blank=True,null=True)
     slider_image = models.ImageField(upload_to='store_image/sliders_image/',null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
